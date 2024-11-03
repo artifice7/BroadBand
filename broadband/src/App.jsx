edc2d24b8c4e5bc6f418c3c95c067f68045ab5b6
Patel Mark <patelmark5330@gmail.com>
2024-11-03 17:00:20 +0530

    Add tailwind css

--- broadband/src/App.jsx
+++ broadband/src/App.jsx
@@ -1,5 +1,4 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import './App.css'
 import Home from "./Home/home";
 
 function App() {
--- broadband/src/Home/home.jsx
+++ broadband/src/Home/home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Navbar from '../components/navbar';
 
 const Home = () => {
     return (
+        <>
+        <Navbar />
         <div>
-            <h1>Welcome to the Home Page</h1>
+            <h1 className='text-xl font-bold'>Welcome to the Home Page</h1>
             <p>This is the home page of the broadband application.</p>
         </div>
+        </>
     );
 };
 
