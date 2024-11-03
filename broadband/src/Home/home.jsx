c15181a13daeda669d8efdc4a6d2414a4516b914
Patel Mark <patelmark5330@gmail.com>
2024-11-03 18:29:00 +0530

    Add navbar
    Add navbar but it is a demo not perfect more change

--- broadband/src/Home/home.jsx
+++ broadband/src/Home/home.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import Navbar from '../components/navbar';
+import React from "react";
+import Navbar from "../components/navbar";
 
 const Home = () => {
-    return (
-        <>
+  return (
+    <>
         <Navbar />
+      <div className="relative max-w-[1712px] mx-auto pt-24 px-8 pb-8">
         <div>
-            <h1 className='text-xl font-bold'>Welcome to the Home Page</h1>
-            <p>This is the home page of the broadband application.</p>
+          <h1 className="text-xl font-bold">Welcome to the Home Page</h1>
+          <p>This is the home page of the broadband application.</p>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Home;
