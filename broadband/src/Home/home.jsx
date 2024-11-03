8aa85517eea20b5d86f4e1c2cc80357773d3685e
Patel Mark <patelmark5330@gmail.com>
2024-11-03 22:19:12 +0530

    nav update

--- broadband/src/Home/home.jsx
+++ broadband/src/Home/home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <>
         <Navbar />
-      <div className="relative max-w-[1712px] mx-auto pt-24 px-8 pb-8">
+      <div className="relative max-w-[1712px] mx-auto pt-24 px-8 pb-8 ">
         <div>
           <h1 className="text-xl font-bold">Welcome to the Home Page</h1>
           <p>This is the home page of the broadband application.</p>
