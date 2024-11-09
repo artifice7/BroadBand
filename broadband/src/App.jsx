e5ad338d24fa5c2e44f0a2fef6d9a6650b4c126e
Patel Mark <patelmark5330@gmail.com>
2024-11-09 23:37:28 +0530

    normal

--- broadband/src/App.jsx
+++ broadband/src/App.jsx
@@ -1,13 +1,17 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 import Home from "./Home/Home";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+      <Analytics />
+    </div>
   );
 }
 
