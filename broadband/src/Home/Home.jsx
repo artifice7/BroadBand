56cdbd2de8ef3e87cbd822ead139aaba796bd087
Aaditya-Dobariya1 <aadityadobariya@gmail.com>
2024-11-09 11:40:07 +0530

    add more sections

--- broadband/src/Home/Home.jsx
+++ broadband/src/Home/Home.jsx
@@ -7,6 +7,7 @@ import HelpSection from "./HelpSection";
 import FiberSection from "./FiberSection";
 import InfoSection from "./InfoSection";
 import TeamSection from "./TeamSection";
+import FooterSection from "./FooterSection";
 
 const Home = () => {
   return (
@@ -22,6 +23,7 @@ const Home = () => {
       <FiberSection />
       <InfoSection />
       <TeamSection />
+      <FooterSection />
     </>
   );
 };
